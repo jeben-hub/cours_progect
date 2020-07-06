467bfcfd91b2fd235633fbf71c721d917d495e50
anton bondaronok <abondaronok@gmail.com>
2020-07-06 18:55:20 +0300

    123

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
   def activation_needed_email(user)
     @user = user
-    mail(to: user.email, subject: "Account activation")
+    mail(to: user.email, subject: "Account activation").deliver
   end
 
   def activation_success_email(user)
     @user = user
-    mail(to: user.email, subject: "Your account is now activated")
+    mail(to: user.email, subject: "Your account is now activated").deliver
   end
 end
