955a87d9a50f3f329641c7cfececfcb55e9c32c5
anton bondaronok <abondaronok@gmail.com>
2020-07-06 18:38:48 +0300

    123

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,6 @@ Rails.application.configure do
   # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@example.com'}
   config.action_mailer.default_url_options = { :host => 'yourapp.heroku.com' }
 
 
