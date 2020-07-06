c2f12b1b75fcd9fc7e7298e0cd562d2420300946
anton bondaronok <abondaronok@gmail.com>
2020-07-06 19:25:13 +0300

    123

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,14 @@
 class UserMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+  default host: 'majestic-grand-canyon-79796.herokuapp.com'
+
   def activation_needed_email(user)
     @user = user
-    mail(to: user.email, subject: "Account activation").deliver
+    mail(to: user.email, subject: "Account activation")
   end
 
   def activation_success_email(user)
     @user = user
-    mail(to: user.email, subject: "Your account is now activated").deliver
+    mail(to: user.email, subject: "Your account is now activated")
   end
 end
