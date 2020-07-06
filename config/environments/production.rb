dd36d980c3ba7c92d3d3ca2e5610c52d6e11e451
anton bondaronok <abondaronok@gmail.com>
2020-07-06 21:26:23 +0300

    123

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,13 +60,13 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "MordorBlog_production"
 
-  config.action_mailer.default_url_options = { :host => 'majestic-grand-canyon-79796.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'majestic-grand-canyon-79796.herokuapp.com' }
   config.action_mailer.smtp_settings = {
       :address              => 'smtp.gmail.com',
       :port                 => 587,
       :domain               => 'gmail.com',
-      :user_name            => 'fanficmordor@gmail.com',
+      :user_name            => 'fanficmordor',
       :password             => 'master_3791',
       :authentication       => 'login',
       :enable_starttls_auto => true
