73633523d53399ba0885122ea877ca66bc3ffe38
anton bondaronok <abondaronok@gmail.com>
2020-07-10 23:25:31 +0300

    bansai number 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,6 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
